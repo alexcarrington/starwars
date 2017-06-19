52ec1df6a4c9ca6c9af02eff6c5460bd0d2d41af
Alex Carrington <alex.carrington@darksky.net>
2017-06-19 09:08:56 -0400

    Implemented infinite scroll. Capped team at 5.

--- star-wars.js
+++ star-wars.js
@@ -1,8 +1,10 @@
 const base_url = 'https://swapi.co/api/people/',
       ul       = document.querySelector('#characters');
-let   count    = 0;
 
-function add_to_list(character) {
+let currChar   = 1,
+    team_count = 1;
+
+function parse_name(character) {
     if (character) {
         return character.name;    
     }
@@ -24,32 +26,45 @@ function appendTo(parent, child) {
     return parent.appendChild(child);
 }
 
-function loadMore() {
-    console.log(this);
-}
-
-function getCharacters(n, offset) {
-    //fetch all characters from SWAPI
-    const names = [];
-
-    for (let i=1; i<=n; i++) {
-        const url = base_url+(i+offset);
-        names.push(fetch(url).then(handler).then(add_to_list));
-        count++;
+function selector() {
+    if (team_count <= 5) {
+        let el = this.innerText,
+        my_team = document.querySelector('#my_characters');
+        my_team.innerHTML += '<li>' + el + '</li>';
+        team_count++;
     }
-    return Promise.all(names).then(function(data) {
-        data.map(function(name) {
-            if (name) {
-                let li = createElement('li');
-
-                li.innerHTML = name;
-                appendTo(ul, li);
-            }
-        })
-        let current = ul.getElementsByTagName('li').length;
-        ul.addEventListener("click", loadMore, false);
-        return current;
-    });
 }
 
-getCharacters(10,0);
+function get_character(char_num) {
+  //Fetch character from SWAPI
+
+    const url = base_url+char_num;
+    
+    return fetch(url).then(handler).then(parse_name);
+}
+
+function loadMore() {
+  // Add 10 more characters
+
+    for (let i=currChar; i<currChar+10; i++) {
+      let li = document.createElement('li');
+      li.addEventListener('click', selector, false);
+      get_character(i).then(function(data) {
+          if (data) {
+              li.innerText = data;
+              ul.appendChild(li);
+              currChar++;
+          }
+      });
+    }
+}
+
+// Detect when scrolled to bottom
+ul.addEventListener('scroll', function() {
+    if (ul.scrollTop + ul.clientHeight >= ul.scrollHeight && currChar <=87) {
+      loadMore();
+    }
+});
+
+// Load first 10 characters
+loadMore();
