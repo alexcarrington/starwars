2d345207eb69fc99f12a3a8fa435ba84dce0dba4
Alex Carrington <alex.carrington@darksky.net>
2017-06-19 13:37:41 -0400

    Added in deselection functionality.

--- star-wars.js
+++ star-wars.js
@@ -1,15 +1,8 @@
-const base_url = 'https://swapi.co/api/people/',
-      ul       = document.querySelector('#characters');
+const base_url  = 'https://swapi.co/api/people/',
+      ul        = document.querySelector('#characters'),
+      curr_team = [];
 
-let currChar   = 1,
-    team_count = 1;
-
-function parse_name(character) {
-    if (character) {
-        return character.name;    
-    }
-    return;
-}
+let curr_char   = 1;
 
 function handler(response) {
     if (response.ok) {
@@ -26,45 +19,62 @@ function appendTo(parent, child) {
     return parent.appendChild(child);
 }
 
+function deselector() {
+    let el       = this.innerText,
+        my_team  = document.querySelector('#my_characters'),
+        el_index = curr_team.indexOf(el);
+
+    my_team.removeChild(this);
+    curr_team.splice(el_index, 1);
+}
+
 function selector() {
-    if (team_count <= 5) {
-        let el = this.innerText,
-        my_team = document.querySelector('#my_characters');
-        my_team.innerHTML += '<li>' + el + '</li>';
-        team_count++;
+    if (curr_team.length < 5) {
+        let el      = this.innerText,
+            my_team = document.querySelector('#my_characters');
+
+        if (!(curr_team.includes(el))) {
+            const clone = this.cloneNode(true);
+            clone.addEventListener('click', deselector, false);
+            my_team.appendChild(clone);
+            curr_team.push(el);
+        }
     }
 }
 
 function get_character(char_num) {
-  //Fetch character from SWAPI
+  // Fetch character from SWAPI
 
     const url = base_url+char_num;
     
-    return fetch(url).then(handler).then(parse_name);
+    return fetch(url).then(handler);
 }
 
-function loadMore() {
+function load_more() {
   // Add 10 more characters
 
-    for (let i=currChar; i<currChar+10; i++) {
+    for (let i=curr_char; i<curr_char+10; i++) {
       let li = document.createElement('li');
+
       li.addEventListener('click', selector, false);
       get_character(i).then(function(data) {
           if (data) {
-              li.innerText = data;
+              li.innerText = data.name;
               ul.appendChild(li);
-              currChar++;
+              curr_char++;
           }
       });
     }
 }
 
-// Detect when scrolled to bottom
+// Detect when scroll reaches bottom
 ul.addEventListener('scroll', function() {
-    if (ul.scrollTop + ul.clientHeight >= ul.scrollHeight && currChar <=87) {
-      loadMore();
-    }
+    get_character('').then(function(data) {
+        if (ul.scrollTop + ul.clientHeight >= ul.scrollHeight && curr_char <= data.count) {
+      load_more();
+        }
+    });
 });
 
 // Load first 10 characters
-loadMore();
+load_more();
